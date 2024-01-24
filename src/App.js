0648cde0463d5aa2b1e4b84fd651e11cfcacc2ec
MONTH4 <goei4559@gmail.com>
2024-01-24 20:06:58 +0900

    [chore] 리액트 프로젝트 생성
    프로젝트 환경 설정

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div> 
+          <Routes>
+              <Route path="/" element={<Home />} />
+          </Routes> 
+      </div>
+    </Router>
+);
+}
+
+export default App;
